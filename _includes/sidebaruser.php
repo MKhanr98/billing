d03dd9a9ef7f99c0a973276bc862a43607c6dcc7
Muhammad Khan Sajjad <mkhanr98@gmail.com>
2023-06-13 15:23:22 +0500

    dont know

--- _includes/sidebaruser.php
+++ _includes/sidebaruser.php
@@ -2,7 +2,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="admin_dashboard.php" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="images/uet_logo.png" alt="UET Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Billing System</span>
     </a>
